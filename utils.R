fbc3130ab3430a511a0e7f7d5cbd84b8dfcebe34
Ruchit10 <rpanchal@broadinstitute.org>
2026-05-06 18:37:36 -0400

    Restructure utils functions and data processing into separate scripts and start making supp fig 2 script

--- utils.R
+++ utils.R
@@ -244,10 +244,6 @@ mc_sections = lapply(annot_sources, function(x) {
   return(tbl)
 })
 names(mc_sections) = annot_sources
-# # Filter out duplicate rows (TODO: Fix bug creating duplicates)
-# mc_sections[["exac"]] = mc_sections[["exac"]][
-#   !(duplicated(paste(mc_sections[["exac"]]$transcript, mc_sections[["exac"]]$section_start))),
-# ]
 mc_section_grs = lapply(mc_sections, function(x) {
   x$chr = x$chr
   x$start = pmin(x$section_start, x$section_end)
@@ -255,6 +251,26 @@ mc_section_grs = lapply(mc_sections, function(x) {
   return(makeGRangesFromDataFrame(x, keep.extra.columns = TRUE))
 })
 
+# Read in gnomAD data
+mc_tbls = lapply(annot_sources, function(x) {
+  tbl = mc_sections[[x]]
+  return(tbl)
+})
+names(mc_tbls) = annot_sources
+
+# Get number of sections per transcript including with no RMC
+transcript_n_sections_lst = lapply(mc_tbls, function(x) {
+  x %>%
+    group_by(transcript) %>%
+    summarize(n_sections = n())
+})
+
+# Join across annotation sources into one table and write out
+transcript_n_sections = transcript_n_sections_lst %>% purrr::reduce(full_join, by = "transcript")
+colnames(transcript_n_sections) =
+  c("transcript", paste0("n_sections_", names(transcript_n_sections_lst)))
+
+transcript_n_sections = as.data.frame(transcript_n_sections[order(transcript_n_sections$transcript), ])
 
 ##############
 ## Missense O/E utilities
